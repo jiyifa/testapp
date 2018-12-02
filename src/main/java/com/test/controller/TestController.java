6960af70036dabd8bb48d4c31ca26544e2e05bf1
jiyifa <jiyifa2017@126.com>
2018-12-03 01:06:17 +0800

    https

--- src/main/java/com/test/controller/TestController.java
+++ src/main/java/com/test/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.test.controller;
 
+import com.test.view.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/test")
     public Object test(){
+        return "test";
+    }
+
+    @RequestMapping("/httpclienttest")
+    public Object httpclienttest(RequestParam requestParam){
+        System.out.println(requestParam.getThreadId()+"\t"+requestParam.getThreadName());
         return "test";
     }
 }
---
+++ src/main/java/com/test/view/RequestParam.java
@@ -0,0 +1,9 @@
+package com.test.view;
+
+import lombok.Data;
+
+@Data
+public class RequestParam {
+    private String threadId;
+    private String threadName;
+}
